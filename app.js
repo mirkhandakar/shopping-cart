daaced43a01fc53c6d1f64e2340e286d554d8819
mirkhandakar <mirkhandakar@gmail.com>
2022-06-14 10:17:15 +0600

    add total

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-document.getElementById('case-plus').addEventListener('click', function () {
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    console.log(caseNumber)
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    //update total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true);
+})
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
+
 })
